2f6faa057bb22028c344d3cd9a56bf97e58ae0bf
Connor Larson <27305718+clarson1229@users.noreply.github.com>
2020-03-09 11:05:46 -0500

    added stuff

---
+++ Assets/forward.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class forward : MonoBehaviour
+{
+    [SerializeField]
+    float pushPower = 750;
+    private void OnCollisionEnter(Collision collision)
+    {
+        Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+        
+        rb.AddForce(10,10,0);
+    }
+}
